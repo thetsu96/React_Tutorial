7cd19adb26bd38c017881fc9587d8a967873ff2e
Thet Su Nwe <thetsunwe96@gmail.com>
2024-05-25 22:19:44 +0630

    Outputting Lists,Props

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,18 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    const [name, setName ] = useState("Mario")
-    const [age, setAge ] = useState(25)
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
-    const handleClick = (e) => {
-        setName("Luigi");
-        setAge(35);
-    }
-
-    return ( 
-        <div className="home">
-            <h2>Homepage</h2>
-            <p> {name} is { age } years old.</p>
-            <button onClick={handleClick}>Click me</button>
-        </div>
-     );
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" />
+    </div>
+  );
 }
  
 export default Home;
