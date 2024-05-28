b00311c48fd60b1eee2455457228f4f18d63a2c7
Thet Su Nwe <thetsunwe96@gmail.com>
2024-05-29 02:00:33 +0630

    Deletin Blogs

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,19 @@
 import { useParams } from 'react-router-dom'
 import useFetch from './useFetch';
+import { useNavigate } from 'react-router-dom';  
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, error, isPending } = useFetch(`http://localhost:8000/blogs/`+id)
-    
+    const history = useNavigate();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+          method: 'DELETE'
+        }).then(() => {
+          history('/');
+        }) 
+    }
+
     return ( 
         <div className="blog-details">
             { isPending && <div>Loading...</div> }
@@ -13,6 +23,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button onClick={handleClick}>delete</button>
                 </article>
             )}
         </div>
